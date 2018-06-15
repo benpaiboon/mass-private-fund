b6a57debe19fa8edb5b48e824abb8767e94bd066
unknown <paiboon@setscope.com>
2018-06-15 19:09:59 +0700

    add html table in (cash.php & instrument.php)

--- cash.php
+++ cash.php
@@ -21,9 +21,31 @@
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
+      echo "<table border='1'>";
+      echo "<thead>";
+      echo "<tr>";
+      echo "<th>Date</th>";
+      echo "<th>Port ID</th>";
+      echo "<th>Title</th>";
+      echo "<th>Market Value</th>";
+      echo "<th>Proportion Pct</th>";
+      echo "</tr>";
+      echo "</thead>";
+      echo "<tbody>";
       while($row = $result->fetch_assoc()) {
-        print_r($row);
+        // echo "<pre>";
+        // print_r($row);
+        // echo "</pre>";
+        echo "<tr>";
+        echo "<td>".$row['date_port']."</td>";
+        echo "<td>".$row['port_id']."</td>";
+        echo "<td>".$row['title']."</td>";
+        echo "<td>".$row['market_value']."</td>";
+        echo "<td>".$row['proportion_pct']."</td>";
+        echo "</tr>";
       }
+      echo "</tbody>";
+      echo "<table>";
     }
     else {
       echo "0 results";
--- instrument.php
+++ instrument.php
@@ -21,9 +21,47 @@
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
+      echo "<table border='1'>";
+      echo "<thead>";
+      echo "<tr>";
+      echo "<th>Date</th>";
+      echo "<th>Port ID</th>";
+      echo "<th>Sector</th>";
+      echo "<th>Securities</th>";
+      echo "<th>Instrument</th>";
+      echo "<th>Unit</th>";
+      echo "<th>Avg Cost Price</th>";
+      echo "<th>Avg Cost</th>";
+      echo "<th>Market Price</th>";
+      echo "<th>Market Value</th>";
+      echo "<th>Unrealized Profit Lost</th>";
+      echo "<th>Unrealized Profit Lost Pct</th>";
+      echo "<th>Proportion Pct</th>";
+      echo "</tr>";
+      echo "</thead>";
+      echo "<tbody>";
       while($row = $result->fetch_assoc()) {
-        print_r($row);
+        // echo "<pre>";
+        // print_r($row);
+        // echo "</pre>";
+        echo "<tr>";
+        echo "<td>".$row['date_port']."</td>";
+        echo "<td>".$row['port_id']."</td>";
+        echo "<td>".$row['sector']."</td>";
+        echo "<td>".$row['securities']."</td>";
+        echo "<td>".$row['instrument']."</td>";
+        echo "<td>".$row['unit']."</td>";
+        echo "<td>".$row['avg_cost_price']."</td>";
+        echo "<td>".$row['avg_cost']."</td>";
+        echo "<td>".$row['market_price']."</td>";
+        echo "<td>".$row['market_value']."</td>";
+        echo "<td>".$row['unrealized_profit_lost']."</td>";
+        echo "<td>".$row['unrealized_profit_lost_pct']."</td>";
+        echo "<td>".$row['proportion_pct']."</td>";
+        echo "</tr>";
       }
+      echo "</tbody>";
+      echo "<table>";
     }
     else {
       echo "0 results";
